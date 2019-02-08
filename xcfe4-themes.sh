9fe32a2fe5791bafd3058e252636d69c5c48291c
dapostal <dapostal@transparent.com>
2019-02-08 16:43:47 +0000

    added terminal themes

---
+++ xcfe4-themes.sh
@@ -0,0 +1,2 @@
+curl -fLo ~/.local/share/xfce4/terminal/colorschemes/gruvbox-dark.theme --create-dirs \
+    https://raw.githubusercontent.com/morhetz/gruvbox-contrib/master/xfce4-terminal/gruvbox-dark.theme
