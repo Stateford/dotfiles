ef53fc53dea95e9104e2692da9650e0c056c2ac6
Dimitri Apostal <dapostal@transparent.com>
2020-01-16 11:43:18 -0500

    Moved files to config folder

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ curl -fLo ~/.local/share/xfce4/terminal/colorschemes/gruvbox-dark.theme --create
 cp ./.vimrc ~/.vimrc
 cp ./.zshrc ~/.zshrc
 
+sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt-get update -y
 # TODO: must add i3 gaps
-sudo apt-get install clang rxvt-unicode i3 i3-wm i3blocks fonts-hack fonts-firacode fonts-fantasque-sans compton -y
+sudo apt-get install clang rxvt-unicode i3-gaps i3blocks fonts-hack fonts-firacode fonts-fantasque-sans compton fonts-font-awesome git-core bash-completion -y
